b355fa34dea0808a0de088f66d89441d012109e8
loganstottle <adsfov@gmail.com>
2025-04-04 18:17:34 -0500

    wahtever

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    void* data;
+    int elem_size, length, capacity;
+} DynamicArray;
+
+DynamicArray* da_new() {
+
+}
+
+int main(void) {
+    
+
+    return 0;
+}
